c12bc0965fda5b61858f481db0d41c220909bf68
AndrePonce3322 <andreponce417@gmail.com>
2023-06-11 03:57:20 -0600

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'code-editor';
+  state: string = 'html';
+
+  @ViewChild('HTML', { static: false }) HtmlEditor!: ElementRef;
+  @ViewChild('CSS', { static: true }) CSSEditor!: ElementRef;
+  @ViewChild('JS', { static: true }) JsEditor!: ElementRef;
+
+  constructor() {
+
+  }
+
+  setState(newState: string) {
+    this.state = newState;
+  }
 }
